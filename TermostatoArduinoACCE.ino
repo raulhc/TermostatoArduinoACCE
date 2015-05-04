41e3fed5fce464c887431dd1ee3a1b1022864d64
raulhc <raulheviacasielles@MacBook-Pro-de-Raul-2.local>
2015-05-05 00:32:13 +0200

    Fichero de configuración
    Poder guardar la configuración en fichero para evitar la perdida ante
    un posible reinicio del Arduino.

--- TermostatoArduinoACCE.ino
+++ TermostatoArduinoACCE.ino
@@ -5,11 +5,14 @@
 ***********************************************/
 #include <Process.h>
 #include <OneWire.h>
+#include <FileIO.h>
 #include <DallasTemperature.h>
 
 #define ONE_WIRE_BUS       7 // Pin usado para conectar con los dispositivos 1-wire, en este caso el sensor de temperatura Ds18b20
 #define HEATING_DEVICE_PIN 8 // Pin digital de salida que activara el rele que acciona el dispositivo para calentar
 #define COOLING_DEVICE_PIN 9 // Pin digital de salida que activara el rele que acciona el dispositivo para enfriar
+
+#define CONFIG_FILE "/mnt/sd/termostatoACCE.config" // Fichero de configuracion
 
 OneWire oneWire(ONE_WIRE_BUS);       // Creamos objeto para comunicarnos con dispositivos 1-wire.
 DallasTemperature sensors(&oneWire); // Creamos objeto para comunicarnos con sensores de temperatura.
@@ -20,6 +23,7 @@ int    _actionMode        = 0;    // Indicara el modo de accion: -1 Enfriar, 0 S
 String _temperatureTime   = "";   // Fecha y hora en la que se produjo la ultima lectura de temperatura
 float  _temperatureMargin = 0.5;  // Margen admisible en el mantenimiento de la temperatura
 
+
 /**
   * Configuracion de Arduino
   */
@@ -27,6 +31,9 @@ void setup() {
   
   // Inicializar el puente de comunicacion entre Arduino<->Linux
   Bridge.begin();
+  
+  // Inicializar libreria para manejo de ficheros en la parte Linux
+  FileSystem.begin();
   
   // Establecemos los pines de accion calor / frio como de salida
   pinMode(HEATING_DEVICE_PIN, OUTPUT);
@@ -36,6 +43,8 @@ void setup() {
   Serial.begin(9600); 
   while(!Serial); // Esperar a que la comunicacion serie este activa
 
+  // Leemos los datos de configuracion del fichero "termostatoACCE.config" que tenemos en la SD
+  readConfigFile();
 }
 
 /**
@@ -149,4 +158,85 @@ void readServerTime() {
     char c = time.read();
     if (c != '\n') _temperatureTime += c;
   }
+}
+
+/**
+  * Lee la configuracion del fichero guardado en la SD. 
+  * Si el fichero no existe se crea con los valores de configuracion por defecto.
+  */
+void readConfigFile() {
+  
+  // Si el fichero no existe entonces se crea con los valores por defecto de configuracion
+  
+  if (!FileSystem.exists(CONFIG_FILE)) {
+      writeConfigFile();
+  } else {
+    // Abrimos el fichero para lectura
+    File configFile = FileSystem.open(CONFIG_FILE, FILE_READ);
+    
+    // Si tenemos acceso al fichero leemos la configuracion
+    if (configFile) {
+      String property1, property2;
+      float value1, value2;
+      
+      // El orden podria variar, asi que leemos los dos posible datos
+      readConfigData(configFile, property1, value1);
+      readConfigData(configFile, property2, value2);
+      if (property1 == "temperatureSet") {
+        _temperatureSet    = value1;
+        _temperatureMargin = value2;
+      } else {
+        _temperatureSet    = value2;
+        _temperatureMargin = value1;
+      } 
+      
+    configFile.close();
+    } 
+  } 
+  Serial.print("--Configuracion--\n\ntemperatureSet = ");
+  Serial.println(_temperatureSet);
+  Serial.print("temperatureMargin = ");
+  Serial.println(_temperatureMargin);
+  Serial.println();
+}
+
+/**
+  * Escribe los datos de configuracion a un fichero. Se almacenan:
+  * - temperatureSet
+  * - temperatureMargin
+  */
+void writeConfigFile() {
+  
+  // Creamos el fichero para escritura, si existe se sobreescribe
+  File configFile = FileSystem.open(CONFIG_FILE, FILE_WRITE);
+  
+  // Si se ha inicializado correctamente el acceso al fichero
+  if (configFile) {
+    writeConfigData(configFile, "temperatureSet", _temperatureSet);
+    writeConfigData(configFile, "temperatureMargin", _temperatureMargin);
+    configFile.close();
+  }
+}
+
+/**
+  * Lee el siguiente dato de configuracion
+  */
+void readConfigData(File& file, String& property, float& value) {
+  String strValue;
+  
+  file.readStringUntil('[');
+  property = file.readStringUntil('=');
+  strValue = file.readStringUntil(']');  
+  value = strValue.toFloat();
+}
+
+/**
+  * Escribe un dato de configuracion
+  */
+void writeConfigData(File& file, String property, float value) {
+  file.print("[");
+  file.print(property);
+  file.print("=");
+  file.print(value);
+  file.print("]\n");
 }
